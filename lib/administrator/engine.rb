caef21fbe4b42686cac7171bf3266aebc37c43fc
Franco Brusatti <fbrusatti@gmail.com>
2011-03-03 02:11:25 -0300

    added blueprint css framework and styling log in page

--- lib/administrator/engine.rb
+++ lib/administrator/engine.rb
@@ -3,5 +3,9 @@ require "rails"
 module Administrator
   class Engine < Rails::Engine
 #    engine_name "administrator"
+
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
   end
 end
